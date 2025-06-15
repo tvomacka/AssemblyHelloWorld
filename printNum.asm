6b8547d416a670cfd45a6368e6f8ef80344714b2
vomicz <t.vomacka@seznam.cz>
2025-06-15 21:17:22 +0200

    convert proc stump

--- printNum.asm
+++ printNum.asm
@@ -6,6 +6,7 @@ extern ExitProcess:proc
 
 .data
 allocSize dq 32	; Size of the memory to allocate (4 bytes for int + 28 bytes for string)
+num dq 42	; The number to convert to a string
 
 .code
 main proc
@@ -17,9 +18,25 @@ main proc
 	mov		r8, allocSize ; Size of the memory to allocate
 	call	HeapAlloc ; Allocate memory from the heap
 
+	sub		rsp, 40h ; Allocate space for the string (32 bytes)
+	mov		rcx, num ; Load the number to convert into rcx
+	mov 	rdx, rax ; Pointer to the allocated memory for the string in rdx
+	call	convert ; Convert the number to a string
+	add		rsp, 40h ; Adjust stack pointer for alignment
+
 	; Clean up and exit
 	add		rsp, 40h ; Deallocate stack space
 	xor		ecx, ecx ; Set exit code to 0
 	call	ExitProcess ; Exit the process
 main endp
+
+; Convert a number to a string representation
+; rcx - the number to convert
+; rdx - pointer to the allocated memory for the string
+; returns the number of characters written in rax
+convert proc
+	xor		rax, rax ; Clear rax for character count
+	
+convert endp
+
 end
