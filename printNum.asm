6af578381188f2bab7a630e26c4bbb114e068753
vomicz <t.vomacka@seznam.cz>
2025-06-15 21:53:21 +0200

    More logic added to the conversion

--- printNum.asm
+++ printNum.asm
@@ -6,7 +6,7 @@ extern ExitProcess:proc
 
 .data
 allocSize dq 32	; Size of the memory to allocate (4 bytes for int + 28 bytes for string)
-num dq 42	; The number to convert to a string
+num dq 2	; The number to convert to a string
 
 .code
 main proc
@@ -33,15 +33,29 @@ main endp
 ; Convert a number to a string representation
 ; rcx - the number to convert
 ; rdx - pointer to the allocated memory for the string
-; returns the number of characters written in rax
+; returns the number of characters written in rax, without the trailing \0 (this might be wrong, check when outputing to console)
 convert proc
 	mov		r8, rdx	; store the memory pointer
 	xor		r9, r9	; how many numbers have been written
-	xor		rdx, rdx
+	
 	mov		rax, rcx	;rdx:rax are to be divided
+Divide:
+	xor		rdx, rdx
 	mov		r10, 10	; divide by base 10
 
 	div		r10
+
+	add		dl, '0'	; char representation of the resulting remainder
+	mov		[r8+r9], dl	; write the resulting char to memory + offset
+	inc		r9	; increase the offset
+
+	cmp		rax, 0
+	jne		Divide	; if there's more to divide, keep going
+
+	;otherwise, write the ending \0 and exit
+	xor		dl, dl
+	mov		[r8+r9], dl
+	mov		rax, r9
 
 	ret
 convert endp
